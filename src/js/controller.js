9cb38cdfc82d64919f6663cdae7476d33eca3ebb
Mykola Danyliuk <mykola.danyliuk@eliftech.com>
2023-06-15 15:58:41 +0300

    added search controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,9 @@
-const recipeContainer = document.querySelector('.recipe');
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -8,6 +13,41 @@ const timeout = function (s) {
   });
 };
 
-// https://forkify-api.herokuapp.com/v2
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
 
-///////////////////////////////////////
+    if (!id) return;
+
+    recipeView.renderSpinner();
+
+    // 1) Loading recipe
+    await model.loadRecipe(id);
+
+    // 2) Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (error) {
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    //1) get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+};
+init();
